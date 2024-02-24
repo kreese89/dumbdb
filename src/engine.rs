cc04a8721e9c1bd155bbadf20c9a23e6cb253752
Kyle Reese <kylereese@Kyles-MacBook-Pro-2.local>
2024-02-23 20:59:59 -0500

    check in a bunch of Engine code + create dir on engine startup

--- src/engine.rs
+++ src/engine.rs
@@ -1,25 +1,63 @@
-use std::str::FromStr;
-
+use std::fs::{create_dir_all, File};
+use std::path::{Path, PathBuf};
 // Engine represents the storage engine for our dumb DB.
-// This is the thing that handles actual reads and writes to the underlying disk
-enum EngineType {
-    Naive,
+// This is the thing that handles actual reads and writes to disk
+
+enum EngineFileOrientation {
+    Log,
+    Page,
 }
 
-impl FromStr for EngineType {
-    fn from_str(input: &str) -> Result<EngineType, ()> {
-        match input.to_lowercase() {
-            "naive" => Ok(EngineType::Naive()),
-            _ => Err(()),
-        }
+// Trait represents the general functionality of a dumbdb Storage Engine
+pub trait Engine {
+    fn read(&self, key: String) -> Result<(), ()>;
+    fn write(&self, key: String, val: String) -> Result<(), ()>;
+    fn shutdown(&self) -> Result<(), ()>; // used on app quit or signal handling
+}
+
+// shared config between Engine implementations
+pub struct EngineConfig {
+    db_directory: PathBuf, // directory where things are written to
+    files: Option<Vec<File>>,
+    orientation: EngineFileOrientation, // TODO: do we actually need this?
+}
+
+pub struct NaiveEngine {
+    config: EngineConfig,
+}
+
+impl Engine for NaiveEngine {
+    fn read(&self, key: String) -> Result<(), ()> {
+        return Ok(());
+    }
+
+    fn write(&self, key: String, val: String) -> Result<(), ()> {
+        return Ok(());
+    }
+
+    fn shutdown(&self) -> Result<(), ()> {
+        return Ok(());
     }
 }
 
-struct Engine {
-    // represents the "type" of engine we are using.
-    // This is so we can switch between different implementations easily
-    // TODO: look into refactoring this so that each engine type is its own 
-    // data type but they all implement some shared functionality
-    // and we can just use them generically
-    engine_type: EngineType,
+impl NaiveEngine {
+    pub fn new() -> NaiveEngine {
+        let config = EngineConfig {
+            // db_directory: String::from("./dumbdb/data/"),
+            db_directory: PathBuf::from("./dumbdb/data/"),
+            orientation: EngineFileOrientation::Log,
+            files: None,
+        };
+        let path = Path::new("./dumbdb/data/");
+        create_dir_all(path).expect("Unable to create data directory for dumbdb");
+
+        return NaiveEngine { config };
+    }
+}
+
+pub fn create_engine_from_string(engine_type: String) -> Option<Box<dyn Engine>> {
+    match engine_type.to_ascii_lowercase().as_str() {
+        "naive" => Some(Box::new(NaiveEngine::new())),
+        _ => None,
+    }
 }
